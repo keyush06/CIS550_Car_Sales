7dfa56fb75c6a4c5538d7203c4281cbabf711450
agasheadwait <agasheadwait24@gmail.com>
2024-04-20 15:48:03 -0400

    Minor updates

--- client/src/App.js
+++ client/src/App.js
@@ -10,54 +10,54 @@ import "bootstrap/dist/css/bootstrap.min.css"; // Importing Bootstrap CSS
 function App() {
   return (
     <Router>
-      <div className="container mt-5">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light rounded">
-          <div className="container-fluid">
-            <Link className="navbar-brand" to="/">
-              WheelWise
-            </Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <Link className="nav-link active" aria-current="page" to="/">
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/search-by-price">
-                    Search By Price
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/search-by-location">
-                    Search By Location
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/advanced-search">
-                    Advanced Search
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/compare-cars">
-                    Compare Cars
-                  </Link>
-                </li>
-              </ul>
-            </div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            <b>WheelWise</b>
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/search-by-price">
+                  Search By Price
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/search-by-location">
+                  Search By Location
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/advanced-search">
+                  Advanced Search
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/compare-cars">
+                  Compare Cars
+                </Link>
+              </li>
+            </ul>
           </div>
-        </nav>
+        </div>
+      </nav>
+      <div className="container mt-5">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/search-by-price" element={<SearchByPrice />} />
